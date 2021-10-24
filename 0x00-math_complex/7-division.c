777888e5eda770edda951df3c13d63b2d253adda
khalil hassayoun <70943561+khalil-hassayoun@users.noreply.github.com>
2021-10-24 17:10:59 +0100

    " "

--- 0x00-math_complex/7-division.c
+++ 0x00-math_complex/7-division.c
@@ -1,16 +1,7 @@
+#include <stdio.h>
 #include "holberton.h"
-
-/**
- * division - returns the modulus
- * @c1: the complex
- * @c2: the complex 2
- * @c3: the complex 3
- * Return: the modulus of complex
- */
-
 void division(complex c1, complex c2, complex *c3)
 {
-c3->re = ((c1.re * c2.re) + (c1.im * c2.im)) / (pow(c2.re, 2) + pow(c2.im, 2));
-c3->im = ((c1.im * c2.re) - (c1.re * c2.im)) / (pow(c2.re, 2) + pow(c2.im, 2));
-
+    c3->re = (c1.re * c2.re + c1.im * c2.im) / (c2.re * c2.re + c2.im * c2.im);
+    c3->im = (-c1.re * c2.im + c1.im * c2.re) / (c2.re * c2.re + c2.im * c2.im);
 }
